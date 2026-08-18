e5e3b2348868e74154910eda0cf9cf09a9c77c41
Luke Gareth Ribchester <luke@luke.gr>
2026-08-18 19:57:51 +0200

    wip: refine title
    Signed-off-by: Luke Gareth Ribchester <luke@luke.gr>

--- addons/lgr/__manifest__.py
+++ addons/lgr/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "LGR",
     "summary": "Custom document layout for Accounting and Sales reports",
-    "version": "1.3.1",
+    "version": "1.3.2",
     "category": "Technical",
     "author": "Luke Gareth Ribchester",
     "license": "LGPL-3",
